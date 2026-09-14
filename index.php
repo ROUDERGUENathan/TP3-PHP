ba9c8b153ac463bccc24bfb6c0690ffa9c2b63be
ROUDERGUENathan <roudergue.nathan@gmail.com>
2026-09-14 10:18:56 +0200

    Update comment formatting in index.php

--- index.php
+++ index.php
@@ -12,7 +12,7 @@
         $utilisateur = 'root';
         $mot_passe = '';
 
-        // 1./ Se connecter au serveur (avec gestion des erreurs par exceptions)
+        //1 Se connecter au serveur (avec gestion des erreurs par exceptions)
         try {
             $connexion = new PDO("mysql:host=$serveur;dbname=$db", $utilisateur, $mot_passe);
             if ($connexion) echo 'Connexion réussie';
@@ -21,7 +21,7 @@
             die('<br/>Erreur : '.$event->getMessage());
         }
 
-        // 3./ Insérer des données
+        //3 Insérer des données
         $name = 'BOLANOS';
         $surname = 'Amandine';
         $phone_number = '0606060606';
@@ -33,7 +33,7 @@
             echo '<br/>Erreur : '.$table_erreurs[2];
         }
 
-        // 4./ Modifier des données
+        //4 Modifier des données
         $name = 'VAUBOURG';
         $surname = 'Mandel';
         $phone_number = '0606060616';
@@ -47,7 +47,7 @@
             echo '<br/>Erreur : '.$table_erreurs[2];
         }
 
-        // 5./ Supprimer des données
+        //5 Supprimer des données
         $supprimer = "DELETE FROM etudiants WHERE idEtudiants>2";
         $supprimer = $connexion->exec($supprimer);
         if ($supprimer) echo '<br/>Suppression effectuée';
@@ -56,7 +56,7 @@
             echo '<br/>Erreur : '.$table_erreurs[2];
         }
 
-        // 6./ Afficher des données
+        //6 Afficher des données
         $afficher = "SELECT * FROM etudiants";
         $afficher = $connexion->query($afficher);
         while ($res = $afficher->fetch(PDO::FETCH_OBJ))
